17f2cb5d0829c4879f29977cc99d2cb89aced5de
Flora Jiang <avril_99@vii.im>
2019-06-10 22:25:00 -0700

    NodeJS Notes and Exercises

---
+++ Server-side Development with NodeJS, Express and MongoDB/Exercises/NodeJS/index.js
@@ -0,0 +1,18 @@
+var rect = {
+    perimeter:(x,y) => (2*(x+y)),
+    area:(x,y) =>(x*y)
+};
+
+function solve_rect(l,b){
+  console.log('Solving for rect with l = ' +l +' and b = '+ b );
+  if (l<=0 || b<=0){
+    console.log('Rect dim should be greater than 0');
+  }
+  else {
+    console.log('The area of the rect is '+ rect.area(l,b));
+    console.log('The perimter of the rect is '+ rect.perimeter(l,b));
+  }
+}
+
+solve_rect(3,6)
+solve_rect(199,22)
---
+++ Server-side Development with NodeJS, Express and MongoDB/NodeJS.md
@@ -0,0 +1,21 @@
+#### Node Modules
+
+##### 3 kinds of Node
+
+- file-based Module
+- core Modules
+  - part of core Node
+  - path, fs, os, util...
+- external Node modules
+  - 3th party modules,
+  - installed using NPM
+  - node_modules folder in your Node application
+
+##### Using Node Modules
+
+By using require function
+
+File-based: require('./module_name')
+
+core/External: require('module_name')
+
